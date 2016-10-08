d32ec50a575d752b92624fbc1ec6cbf1cb8e355b
Ramiro Bou <bouramiro@gmail.com>
2016-10-07 22:36:29 -0300

    Adding example config file

---
+++ example.config.py
@@ -0,0 +1,30 @@
+# IRC server details
+nick = "jenniffay"
+server = "irc.goat.chat"
+port = 6697
+tls = True
+
+# Nickserv user and password
+user = "jenni"
+password = "hunter2"
+
+# Bot admins (uses account name)
+admins = ['Polsaker']
+
+# Channels we'll autojoin
+channels = ['#jenni']
+
+# Prefix for commands
+prefix = "."
+
+# We'll send error messages and other stuff here
+logchannel = "#jenni"
+
+# Modules we will NOT load
+disabled_modules = ['chat', 'wolframalpha', 'rss']
+
+# URLs we won't give titles for (for the title module)
+urlblacklist = ['git.io', 'bpaste.net', 'bitbucket.org']
+
+# WolframAlpha API key (for wolframalpha module)
+wolframalpha_apikey = "123456-7894561234"
