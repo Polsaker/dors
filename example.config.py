0ea475b7fc8334d453b65c05ba729ccce571aa5b
pembo210 <pembo210@users.noreply.github.com>
2017-08-09 22:34:55 -0400

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -24,7 +24,7 @@ logchannel = "#jenni"
 disabled_modules = ['chat', 'wolframalpha', 'rss']
 
 # If whitelistonly, only these modules will load
-whitelistonly_modules = ['admin', 'cryptocoin']
+whitelistonly_modules = ['admin', 'reloader']
 
 # URLs we won't give titles for (for the title module)
 urlblacklist = ['git.io', 'bpaste.net', 'bitbucket.org']
