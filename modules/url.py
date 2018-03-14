8c4c61eddfad951d272ed953f08a11b7b60c5c16
pembo210 <pembo210@users.noreply.github.com>
2018-03-13 20:48:10 -0400

    use bs4 for page titles

--- modules/url.py
+++ modules/url.py
@@ -16,7 +16,7 @@ def get_page_backup(url):
     # fix url
     k = urllib.parse.urlparse(url)
     url = k.geturl()
-    
+
     req = urllib.request.Request(url, headers={'Accept':'*/*'})
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0')
     u = urllib.request.urlopen(req)
@@ -58,13 +58,12 @@ def find_title(url, irc):
     if '/#!' in url:
         url = url.replace('/#!', '/?_escaped_fragment_=')
 
-    if 'i.imgur' in url:
-        a = url.split('.')
-        url = a[0][:-1] + '.'.join(a[1:-1])
+    if 'i.imgur' in url: # blacklist?
+        return
 
     if 'zerobin.net' in url:
         return True, 'ZeroBin'
-    
+
     if 'store.steampowered.com/app' in url and 'steam' in irc.plugins:
         appid = re.search('.*store.steampowered.com/app/(\d+).*', url)
         return True, irc.plugins['steam'].getAppInfo(appid.group(1), False)
@@ -74,113 +73,13 @@ def find_title(url, irc):
         soup = BeautifulSoup(r.text, 'lxml')
         title = str(soup.title.string)
         title = title.replace('\n', ' ').replace('\r', '')
+        title = title.replace('"', '')
         return True, "{0}".format(title)
 
-
-    #url = url.decode()
-    msg = str()
-    k = 0
-    status = False
-
-    while not status:
-        k += 1
-        if k > 3:
-            break
-
-        msg = dict()
-        status, msg = get_page_backup(url)
-
-        if type(msg) == type(dict()) and 'code' in msg:
-            status = msg['code']
-        else:
-            continue
-        time.sleep(0.5)
-
-
-    if not status:
-        return False, msg
-
-    useful = msg
-
-    info = useful['headers']
-    page = useful['read']
-
-    try:
-        mtype = info['Content-Type']
-    except:
-        print('failed mtype:', str(info))
-        return False, 'mtype failed'
-    if not (('/html' in mtype) or ('/xhtml' in mtype)):
-        return False, str(mtype)
-
-    content = page
-    regex = re.compile('<(/?)title( [^>]+)?>', re.IGNORECASE)
-    content = regex.sub(r'<\1title>', content)
-    regex = re.compile('[\'"]<title>[\'"]', re.IGNORECASE)
-    content = regex.sub('', content)
-    start = content.find('<title>')
-    if start == -1:
-        return False, 'NO <title> found'
-    end = content.find('</title>', start)
-    if end == -1:
-        return False, 'NO </title> found'
-    content = content[start + 7:end]
-    content = content.strip('\n').rstrip().lstrip()
-    title = content
-
-    if len(title) > 200:
-        title = title[:200] + '[...]'
-
-    def e(m):
-        entity = m.group()
-        if entity.startswith('&#x'):
-            cp = int(entity[3:-1], 16)
-            meep = chr(cp)
-        elif entity.startswith('&#'):
-            cp = int(entity[2:-1])
-            meep = chr(cp)
-        else:
-            entity_stripped = entity[1:-1]
-            try:
-                char = name2codepoint[entity_stripped]
-                meep = chr(char)
-            except:
-                if entity_stripped in HTML_ENTITIES:
-                    meep = HTML_ENTITIES[entity_stripped]
-                else:
-                    meep = str()
-        try:
-            return meep.decode()
-        except:
-            return meep.encode().decode()
-
-    title = r_entity.sub(e, title)
-
-    title = title.replace('\n', '')
-    title = title.replace('\r', '')
-
-    def remove_spaces(x):
-        if '  ' in x:
-            x = x.replace('  ', ' ')
-            return remove_spaces(x)
-        else:
-            return x
-
-    title = remove_spaces(title)
-
-    new_title = str()
-    for char in title:
-        if len(list(char.encode())) <= 3:
-            new_title += char
-    title = new_title
-
-    title = re.sub(r'(?i)dcc\ssend', '', title)
-
-    title += '\x0F'
-    if title:
-        return True, title
-    else:
-        return False, 'No Title'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    title = str(soup.title.string)
+    return True, "{0}".format(title)
 
 
 def getTLD(url):
@@ -216,11 +115,9 @@ def get_results(text, irc, manual=False):
         domain = getTLD(url)
         if '//' in domain:
             domain = domain.split('//')[1]
-        if 'i.imgur.com' in url and url.startswith('http://'):
-            url = url.replace('http:', 'https:')
 
         bitly = url
-    
+
         if not url.startswith("!"):
             passs, page_title = find_title(url, irc)
             display.append([page_title, url, bitly, passs])
