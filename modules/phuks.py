798d7aeabb6e2d5d5a85782863b19fb6b226cde9
pembo210 <pembo210@users.noreply.github.com>
2019-02-27 04:51:50 -0500

    adding getSub hook

--- modules/phuks.py
+++ modules/phuks.py
@@ -27,7 +27,29 @@ def get_user(name):
     resp += "Registered \002{0}\002 ago, ".format(format_timedelta(del_regist, locale='en_US'))
     resp += "mods {0} and owns {1} subs. https://phuks.co/u/{2}".format(len(usdata['mods']), len(usdata['owns']), usdata['name'])
     return resp
-    
+
+
+def get_sub(name):
+    try:
+        usdata = requests.get('https://phuks.co/api/getSub/{0}'.format(name))
+    except:
+        return "Could not contact Phuks"
+    if usdata.status_code != 200:
+        return "Could not contact Phuks"
+    usdata = usdata.json()
+    if usdata['status'] != "ok":
+        return "Error: " + usdata['error']
+    usdata = usdata['sub']
+    resp = "Sub \002{0}\002".format(usdata['name'])
+    if usdata['nsfw']:
+        resp += " [NSFW]"
+    regist = datetime.datetime.strptime(usdata['created'], "%Y-%m-%d %H:%M:%S.%f")
+    del_regist = datetime.datetime.utcnow() - regist
+    resp += " Created \002{0}\002 ago by {1},".format(format_timedelta(del_regist, locale='en_US'), usdata['creator'])
+    resp += " {0} posts, {1} subscribers, {2} mods".format(usdata['posts'], usdata['subscribers'], len(usdata['mods']))
+    return resp
+
+
 @commandHook(['user'])
 def phukuser(irc, ev):
     if ev.target in ("#phuks", "#phukadmins", "#throat", "Polsaker"):
@@ -36,3 +58,11 @@ def phukuser(irc, ev):
 
         irc.message(ev.replyto, get_user(ev.args[0]))
 
+
+@commandHook(['sub'])
+def phuksub(irc, ev):
+    if ev.target in ("#phuks", "#phukadmins", "#throat", "Polsaker"):
+        if not ev.args:
+            return irc.reply("Usage: sub <subname>")
+
+        irc.message(ev.replyto, get_sub(ev.args[0]))
